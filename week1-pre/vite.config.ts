d4d361a814c20ce103f35f649a6b88a873a371ce
Mark Zhang <markzhang1994@gmail.com>
2024-12-22 15:15:24 +0800

    Update deploy script and remove output directory specification in Vite config

--- week1-pre/vite.config.ts
+++ week1-pre/vite.config.ts
@@ -6,7 +6,6 @@ export default defineConfig({
   base: './',
   plugins: [react()],
   build: {
-    outDir: 'dist/week1-pre',
     rollupOptions: {
       output: {
         entryFileNames: 'assets/[name].js',
