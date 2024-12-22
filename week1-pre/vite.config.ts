dee1a8bed21929ef3a6c686adb84075813c1ad6f
Mark Zhang <markzhang1994@gmail.com>
2024-12-22 14:07:30 +0800

    Change build folder to fit gh-pages

--- week1-pre/vite.config.ts
+++ week1-pre/vite.config.ts
@@ -3,8 +3,10 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: './',
   plugins: [react()],
   build: {
+    outDir: 'dist/week1-pre',
     rollupOptions: {
       output: {
         entryFileNames: 'assets/[name].js',
